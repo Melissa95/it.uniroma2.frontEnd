134bf174b1573be1b7feb5501e2de1ef90320aed
Emanuele <emanuelecamarda@gmail.com>
2018-07-13 11:21:09 +0200

    possibilità modifica indirizzo IP BE

--- app/js/serviceAjax.js
+++ app/js/serviceAjax.js
@@ -1,4 +1,7 @@
-app.service("myAjax", function($q,$http) {
+app.service("myAjax", function($q,$http,$scope) {
+
+    $scope.BEIpAddress = "localhost";
+    $scope.URL = "http://" + BEIpAddress + ":8200/ticketingsystem/";
 
     var ajax = function(method,url,data) {
 
@@ -25,29 +28,29 @@ app.service("myAjax", function($q,$http) {
 
 
     this.signInUser = function(data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/user/"+ data.username, data);
+        return ajax("POST", URL + "user/"+ data.username, data);
     };
 
     this.modifyUser = function (data) {
-        return ajax("PUT", "http://localhost:8200/ticketingsystem/user/"+ data.username, data);
+        return ajax("PUT", user/"+ data.username, data);
 
     };
 
     this.createTicket = function (data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/ticket/", data);
+        return ajax("POST", URL + "ticket/", data);
 
     };
 
     this.getTickets = function(params) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket", params);
+        return ajax("GET", URL + "ticket", params);
     };
 
     this.getTicketsForRel = function(params) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket/findTicketForCreateEquality", params);
+        return ajax("GET", URL + "ticket/findTicketForCreateEquality", params);
     };
 
     this.getTargets = function (params) {
-        return ajax("GET","http://localhost:8200/ticketingsystem/target",params);
+        return ajax("GET",URL + "target",params);
 
     };
 
@@ -57,95 +60,95 @@ app.service("myAjax", function($q,$http) {
     };
 
     this.createTarget = function (data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/target", data);
+        return ajax("POST", URL + "target", data);
 
     };
 
     this.newRelation = function (data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/relation/"+ data.name, data);
+        return ajax("POST", URL + "relation/"+ data.name, data);
 
     };
 
     this.escalation = function (data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/escalation", data);
+        return ajax("POST", URL + "escalation", data);
 
     };
 
     this.getAllRel = function(params) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/relation", params);
+        return ajax("GET", URL + "relation", params);
     };
 
     this.getTicketForDep = function (params) {
-        return ajax("GET","http://localhost:8200/ticketingsystem/ticket/findTicketForCreateDependency",params);
+        return ajax("GET",URL + "ticket/findTicketForCreateDependency",params);
 
     };
 
     this.getTicketForReg = function (params) {
-        return ajax("GET","http://localhost:8200/ticketingsystem/ticket/findTicketForCreateRegression",params);
+        return ajax("GET",URL + "ticket/findTicketForCreateRegression",params);
 
     };
 
     this.getTicketForEqual = function (params) {
-        return ajax("GET","http://localhost:8200/ticketingsystem/ticket/findTicketForCreateEquality",params);
+        return ajax("GET",URL + "ticket/findTicketForCreateEquality",params);
 
     };
 
     this.putRelationEqual = function (data,choose) {
-        return ajax("PUT", "http://localhost:8200/ticketingsystem/ticket/addEqualityTicket/"+ choose + "/" + data.sameTicket.id, data);
+        return ajax("PUT", URL + "ticket/addEqualityTicket/"+ choose + "/" + data.sameTicket.id, data);
 
     };
 
     this.putRelationDep = function (data,choose,idChoose) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/ticket/addDependentTicket/"+ idChoose + "/" + choose, data);
+        return ajax("POST", URL + "ticket/addDependentTicket/"+ idChoose + "/" + choose, data);
 
     };
 
     this.putRelationRegression = function (data,choose,idChoose) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/ticket/addRegression/" + choose + "/" + idChoose, data);
+        return ajax("POST", URL + "ticket/addRegression/" + choose + "/" + idChoose, data);
 
     };
 
     this.putRelationCustom = function (data) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/relationInstance/" + data.relation.name + "/" + data.fatherTicket.id + "/" + data.sonTicket.id, data);
+        return ajax("POST", URL + "relationInstance/" + data.relation.name + "/" + data.fatherTicket.id + "/" + data.sonTicket.id, data);
 
     };
 
     this.getQueue = function (data) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket/findTicketInQueue", data);
+        return ajax("GET", URL + "ticket/findTicketInQueue", data);
 
     };
 
     this.getDetailsTicket = function (data,idTicket) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket/" + idTicket, data);
+        return ajax("GET", URL + "ticket/" + idTicket, data);
 
     };
 
     this.getRelationTicket = function (data) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/relation", data);
+        return ajax("GET", URL + "relation", data);
 
     };
 
     this.getRelationCustomTicket = function (data,ticketId) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/relationInstance/findRelations/" + ticketId, data);
+        return ajax("GET", URL + "relationInstance/findRelations/" + ticketId, data);
 
     };
 
     this.getTicketGantt = function(data,team) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket/findTicketForGantt/" + team , data);
+        return ajax("GET", URL + "ticket/findTicketForGantt/" + team , data);
 
     };
 
     this.getMyTeam = function(data,username) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/team/findAllTeamsByPerson/" + username , data);
+        return ajax("GET", URL + "team/findAllTeamsByPerson/" + username , data);
 
     };
     this.getPlanning = function(data,teamName,date,durat,idtick) {
-        return ajax("POST", "http://localhost:8200/ticketingsystem/gantt/createGanttInstance/" + teamName + "/"+ date +"/"+ durat +"/"+idtick, data);
+        return ajax("POST", URL + "gantt/createGanttInstance/" + teamName + "/"+ date +"/"+ durat +"/"+idtick, data);
 
     };
 
     this.getFatherTicket = function(data,id) {
-        return ajax("GET", "http://localhost:8200/ticketingsystem/ticket/findFatherTicket/" + id, data);
+        return ajax("GET", URL + "ticket/findFatherTicket/" + id, data);
 
     };
 
